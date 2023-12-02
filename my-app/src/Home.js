303ce946656ef8850b1b6102894e0f4f5a7d37de
Harpreet Singh <Kamaljit67802@gmail.com>
2023-12-02 10:19:38 +0530

    Home page

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,10 +1,14 @@
+import { signOut } from "firebase/auth"
+import { auth } from "./firebase"
 
 
 
 export const Home = () => {
+    const user = signOut(auth)
+    console.log(user)
     return (
         <div className="p-4 box mt-3 text-center">
-         <h1>Hello Welcome</h1>   
+         <h1>Hello Welcome {user.email}</h1>   
         </div>
     )
 
